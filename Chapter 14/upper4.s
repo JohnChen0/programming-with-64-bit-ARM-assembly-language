0662a0dcf3e857b8a1de9137b460e3a3de885ba4
John Chen <john_chen@outlook.com>
2025-02-12 06:56:41 +0000

    Ch 14 (optimizing code) updates
    A couple of changes to upper4.s (NEON parallel version of toupper):
    * Initialize Q1, Q3, and Q8 with LD1R instruction, to replicate one
      byte to all 16 lanes.
    * Replace two-instrution sequence CMHI and NOT with a single
      instruction CMHS.

--- Chapter 14/upper4.s
+++ Chapter 14/upper4.s
@@ -15,18 +15,15 @@
 
 	.EQU	N, 4
 toupper:
-	LDR X2, =aaas
-	LDR	Q1, [X2]   // Load Q1 with all as
-	LDR X2, =endch
-	LDR	Q3, [X2]  // Load Q3 with all 25's
-	LDR X2, =spaces
-	LDR	Q8, [X2] // Load Q8 with all spaces
+	LDR X2, =a
+	LD1R	{V1.16B}, [X2], #1   // Load Q1 with all as
+	LD1R	{V3.16B}, [X2], #1  // Load Q3 with all 25's
+	LD1R	{V8.16B}, [X2], #1 // Load Q8 with all spaces
 	MOV	W3, #N
 // The loop is until byte pointed to by R1 is non-zero
 loop:	LDR	Q0, [X0], #16 // load 16 characters and increment pointer
 	SUB	V2.16B, V0.16B, V1.16B	// Subtract 'a's
-	CMHI	V2.16B, V2.16B, V3.16B	// compare chars to 25's
-	NOT     V2.16B, V2.16B          // no CMLO so need to not
+	CMHS	V2.16B, V3.16B, V2.16B	// compare 25's to chars
 	AND V2.16B, V2.16B, V8.16B 	// and result with spaces
 	BIC	V0.16B, V0.16B, V2.16B	// kill the bit that makes it lowercase
 	STR	Q0, [X1], #16	    // store character to output str
@@ -36,7 +33,7 @@ loop:	LDR	Q0, [X0], #16 // load 16 characters and increment pointer
 	RET		// Return to caller
 
 .data
-aaas:    .fill  16, 1, 'a'	// 16 a's
-endch:  .fill	16, 1, 25  	// after shift, chars are 0-25
-spaces:	.fill	16, 1, 0x20     // spaces for bic
+a:	.byte	'a'
+endch:	.byte	25	// after shift, chars are 0-25
+space:	.byte	0x20	// space for bic
 
