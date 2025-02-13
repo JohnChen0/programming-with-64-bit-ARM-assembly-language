a60503338aa3d56771774f57138c24dbcc89f3f1
John Chen <john_chen@outlook.com>
2025-02-13 05:49:43 +0000

    Additional ideas for NEON-based toupper in Ch 14
    * Use DUP instruction to initialize Q1, Q3, and Q8, completely
      eliminating the need to store any data outside of the instruction
      stream.
    * Instead of hardcoding the number of iterations, use a 4-instruction
      sequence to detect the null terminator.

--- Chapter 14/upper4.s
+++ Chapter 14/upper4.s
@@ -4,7 +4,8 @@
 //
 // X0 - address of input string
 // X1 - address of output string
-// X2 - use as indirection to load data
+// X2 - use to store temporary data
+// X3 - saved address of input string, for length calculation
 // Q0 - 8 characters to be processed
 // V1 - contains all a's for comparison
 // V2 - result of comparison with 'a's
@@ -13,27 +14,24 @@
 
 .global toupper	     // Allow other files to call this routine
 
-	.EQU	N, 4
 toupper:
-	LDR X2, =a
-	LD1R	{V1.16B}, [X2], #1   // Load Q1 with all as
-	LD1R	{V3.16B}, [X2], #1  // Load Q3 with all 25's
-	LD1R	{V8.16B}, [X2], #1 // Load Q8 with all spaces
-	MOV	W3, #N
-// The loop is until byte pointed to by R1 is non-zero
+	MOV	W2, #(25 * 0x100 + 'a') // Prepare data for DUP
+	MOVK	W2, #0x20, LSL 16
+	FMOV	S0, W2
+	DUP	V1.16B, V0.16B[0]	// Load Q1 with all as
+	DUP	V3.16B, V0.16B[1]	// Load Q3 with all 25's
+	DUP	V8.16B, V0.16B[2]	// Load Q8 with all spaces
+	MOV	X3, X0
+// The loop is until a zero byte is loaded into Q0
 loop:	LDR	Q0, [X0], #16 // load 16 characters and increment pointer
 	SUB	V2.16B, V0.16B, V1.16B	// Subtract 'a's
 	CMHS	V2.16B, V3.16B, V2.16B	// compare 25's to chars
 	AND V2.16B, V2.16B, V8.16B 	// and result with spaces
 	BIC	V0.16B, V0.16B, V2.16B	// kill the bit that makes it lowercase
 	STR	Q0, [X1], #16	    // store character to output str
-	SUBS	W3, W3, #1		// decrement loop counter and set flags
-	B.NE	loop		// loop if character isn't null
-	MOV	X0, #(N*16)	// get the length by subtracting the pointers
+	CMEQ	V0.16B, V0.16B, #0
+	ADDP	D0, V0.2D
+	FMOV	X2, D0
+	CBZ	X2, loop		// loop if all characters aren't null
+	SUB	X0, X0, X3	// get the length by subtracting the pointers
 	RET		// Return to caller
-
-.data
-a:	.byte	'a'
-endch:	.byte	25	// after shift, chars are 0-25
-space:	.byte	0x20	// space for bic
-
